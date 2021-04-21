b5317d7503e2501081d7807a3da11ac7e3f38e43
Pär Axel Leonard Rosén <parosen@ulrik.uio.no>
2021-04-21 13:26:09 +0200

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# test-environment-nrec
-Scripts, configuration and documentation to deploy test environments on the NREC
+## Setting up a Statum 1 server inside NREC from scratch
+This quide shows how I set up a Stratum 1 server in NREC using Terraform and the OpenStack CLI. For
+this two VMs are needed, one for setting up ansible with EESSI settings, and one for the actual
+Stratum 1 server. Since the ansible VM is just needed to install the Stratum 1 VM, we can use the
+"m1.small" flavor. For the Stratum 1 VM we use the "m1.large" flavor together with an additional
+volume of 200GB. This disksize should be sufficient for a while during the pilot phase. Currently
+(2021-04-21) the whole software stack takes up around 60GB but this is expected to grow quite fast.
+For production use we will need to have a volume of at least 1TB.
+
+### Problems using Terraform
+
+- difficult syntax, hard to get right
+- a lot of destroying and creating to test small changes
+- if you put everything into one script (creating zone, volume, security group) then you have to delete manually if something goes wrong and you need
+  to rerun the script.
+
+I've opted for using Terraform together with OpenStack CLI because some things are easier to just do
+outside of a script.
+
+First you need to clone this repo
+
+```console
+git clone https://github.com/NorESSI/test-environment-nrec.git
+```
+
+Fill in username and password in keystone_rc.sh
+
+```console
+source keystone_rc.sh
+```
+
+Put ssh public keys of project members into the file called authorized_keys. One key for each line.
+
+Create zone (easier to do with openstack command..)
+
+```console
+openstack zone create --email parosen@uio.no nessi-prod.uiocloud.no.
+```
+
+Create silly keypair (to be used only for adding the authorized_keys in /home/centos/.ssh/authorized_keys on the host
+
+```console
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/terraform-keys -q -N ""
+```
+
+Create ansible host in uib-nessi-prod
+```console
+cd eessi-ansible
+```
+```console
+terraform 0.13upgrade && terraform init
+terraform plan
+terraform apply
+```
+
+Delete the terraform-key from the project
+
+```console
+openstack keypair delete terraform-key
+```
+
+Created stratum 1 host in uib-nessi-prod
+
+```console
+cd ../cvmfs-s1-bgo
+terraform 0.13upgrade && terraform init
+terraform plan
+terraform apply
+```
+
+Delete terraform-key again
+
+```console
+openstack keypair delete terraform-key
+```
+
+The VMs eeesi-ansible.nessi-prod.uiocloud.no and cvmfs-s1-bgo-prod.eessi-prod.uiocloud.no are now
+created. They can be logged in using the corresponding private ssh keys.
+
+## cvmfs-s1-bgo-prod VM
+
+TODO
+
+## eessi-ansible VM
+
+TODO
+
+
+
+
+Ref.
+https://docs.nrec.no/terraform-part1.html
+https://docs.nrec.no/terraform-part2.html
+https://docs.nrec.no/terraform-part5.html
+
+https://eessi.github.io/docs/filesystem_layer/stratum1/
+
---
+++ eessi-ansible/basic.tf
@@ -0,0 +1,151 @@
+# Security group
+resource "openstack_networking_secgroup_v2" "instance_access" {
+    name = "ssh-icmp-uio-uib"
+    description = "Allows SSH and ICMP access from UiO and UiB"
+}
+
+# Allow ssh from IPv4 net UiO
+resource "openstack_networking_secgroup_rule_v2" "rule_ssh_access_ipv4_uio" {
+    direction = "ingress"
+    ethertype = "IPv4"
+    protocol  = "tcp"
+    port_range_min = 22
+    port_range_max = 22
+    remote_ip_prefix = "129.240.0.0/16"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+# Allow ssh from IPv4 net UiB
+resource "openstack_networking_secgroup_rule_v2" "rule_ssh_access_ipv4_uib" {
+    direction = "ingress"
+    ethertype = "IPv4"
+    protocol  = "tcp"
+    port_range_min = 22
+    port_range_max = 22
+    remote_ip_prefix = "129.177.0.0/16"
+    #remote_ip_prefix = "158.39.0.0/16"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+# Allow ssh from IPv6 net UiO
+resource "openstack_networking_secgroup_rule_v2" "rule_ssh_access_ipv6_uio" {
+    direction = "ingress"
+    ethertype = "IPv6"
+    protocol  = "tcp"
+    port_range_min = 22
+    port_range_max = 22
+    remote_ip_prefix = "2001:700:100::/40"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+# Allow ssh from IPv6 net UiB
+resource "openstack_networking_secgroup_rule_v2" "rule_ssh_access_ipv6_uib" {
+    direction = "ingress"
+    ethertype = "IPv6"
+    protocol  = "tcp"
+    port_range_min = 22
+    port_range_max = 22
+    remote_ip_prefix = "2001:700:200::/48"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+# Allow icmp from IPv4 net UiO
+resource "openstack_networking_secgroup_rule_v2" "rule_icmp_access_ipv4_uio" {
+    direction = "ingress"
+    ethertype = "IPv4"
+    protocol  = "icmp"
+    remote_ip_prefix = "129.240.0.0/16"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+# Allow icmp from IPv4 net UiB
+resource "openstack_networking_secgroup_rule_v2" "rule_icmp_access_ipv4_uib" {
+    direction = "ingress"
+    ethertype = "IPv4"
+    protocol  = "icmp"
+    remote_ip_prefix = "129.177.0.0/16"
+    #remote_ip_prefix = "158.39.0.0/16"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+# Allow icmp from IPv6 net UiO
+resource "openstack_networking_secgroup_rule_v2" "rule_icmp_access_ipv6_uio" {
+    direction = "ingress"
+    ethertype = "IPv6"
+    protocol = "icmp"
+    remote_ip_prefix = "2001:700:100::/40"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+# Allow icmp from IPv6 net UiB
+resource "openstack_networking_secgroup_rule_v2" "rule_icmp_access_ipv6_uib" {
+    direction = "ingress"
+    ethertype = "IPv6"
+    protocol = "icmp"
+    remote_ip_prefix = "2001:700:200::/48"
+    security_group_id = openstack_networking_secgroup_v2.instance_access.id
+}
+
+resource "openstack_compute_keypair_v2" "tf-keypair" {
+  name       = "terraform-key"
+  public_key = file("~/.ssh/terraform-keys.pub")
+}
+
+resource "openstack_compute_instance_v2" "testnode" {
+    name = "eessi-ansible"
+    image_name = "GOLD CentOS 7"
+    flavor_name = "m1.small"
+    key_pair = openstack_compute_keypair_v2.tf-keypair.name
+    security_groups = ["default","ssh-icmp-uio-uib" ]
+
+    network {
+        name = "dualStack"
+    }
+
+    lifecycle {
+        ignore_changes = [image_name]
+    }
+}
+
+resource "null_resource" "authkeys" {
+  depends_on = [openstack_compute_instance_v2.testnode]
+  provisioner "file" {
+    source      = "../authorized_keys"
+    destination = "/home/centos/.ssh/authorized_keys"
+    }
+  connection {
+    host        = openstack_compute_instance_v2.testnode.network.0.fixed_ip_v4
+    type        = "ssh"
+    user        = "centos"
+    private_key = file("~/.ssh/terraform-keys")
+    timeout     = "45s"
+    }
+}
+
+# existing DNS zone
+variable "nessi_zone_name" {
+  default = "nessi-prod.uiocloud.no"
+}
+
+# Find zone info
+data "openstack_dns_zone_v2" "nessi_zone" {
+  name = "${var.nessi_zone_name}."
+}
+
+
+# Create records for A (IPv4)
+resource "openstack_dns_recordset_v2" "a_records" {
+  zone_id     = data.openstack_dns_zone_v2.nessi_zone.id
+  name        = "${openstack_compute_instance_v2.testnode.name}.${var.nessi_zone_name}."
+  type        = "A"
+  #records     = ["${openstack_compute_instance_v2.testnode.access_ip_v4}"]
+  records     = [openstack_compute_instance_v2.testnode.access_ip_v4]
+}
+
+# Create records for AAAA (IPv6)
+resource "openstack_dns_recordset_v2" "aaaa_records" {
+  zone_id     = data.openstack_dns_zone_v2.nessi_zone.id
+  name        = "${openstack_compute_instance_v2.testnode.name}.${var.nessi_zone_name}."
+  type        = "AAAA"
+  records     = [openstack_compute_instance_v2.testnode.access_ip_v6]
+}
